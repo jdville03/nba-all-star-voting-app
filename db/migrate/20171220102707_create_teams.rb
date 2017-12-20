d53c9b98a79fd460cd98fdfa236280aa85cc1317
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-20 05:30:13 -0500

    Create Team model and migration

---
+++ db/migrate/20171220102707_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[5.1]
+  def change
+    create_table :teams do |t|
+      t.string :city
+      t.string :name
+      t.string :abbreviation
+
+      t.timestamps
+    end
+  end
+end
