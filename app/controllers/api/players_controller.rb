0153429e9d7a26e54610742397661106bac50a3a
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-20 06:16:25 -0500

    Add api/player controller actions

---
+++ app/controllers/api/players_controller.rb
@@ -0,0 +1,50 @@
+class Api::PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :update, :destroy]
+  
+  def index
+    render json: Player.all
+  end
+  
+  def show
+    render json: @player
+  end
+
+  def create
+    player = Player.new(player_params)
+    if player.save
+      render json: player
+    else
+      render json: { message: player.errors }, status: 400
+    end 
+  end
+
+  def update
+    if @player.update(player_params)
+      render json: @player
+    else
+      render json: { message: @player.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @player.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to remove this player" }, status: 400
+    end
+  end
+
+  private
+
+    def player_params
+      params.require(:player).permit(:last_name, :first_name, :jersey_number, :position, :height, :weight, :age, :birth_city, :birth_country, :image_url, :team_id)
+    end
+
+    def set_player
+      @player = Player.find_by(id: params[:id])
+    end
+
+
+
+
+end
