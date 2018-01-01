b4f2452fdc5ba525c380bdc4acc85d003f687be1
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-31 21:57:54 -0500

    Add team_id attribute to PlayerSerializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,4 +1,4 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :last_name, :first_name, :position, :image_url, :votes
+  attributes :id, :last_name, :first_name, :position, :image_url, :votes, :team_id
   belongs_to :team
 end
