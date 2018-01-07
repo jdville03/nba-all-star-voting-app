d8f6ba82d9392518d7a452503c64ff4b5c0dc0ec
Jdville03 <jarrel.delottinville@gmail.com>
2018-01-07 10:04:16 -0500

    Add validations to player and team models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ApplicationRecord
   belongs_to :team
+
+  validates :last_name, :first_name, :position, :votes, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ApplicationRecord
   has_many :players
+
+  validates :city, :name, :abbreviation, :conference, presence: true
 end
